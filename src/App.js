d193597ff9d5b3d9893e2c34715ccc96453e0d6b
fatimzahra1 <fatimzahraelmadini@gmail.com>
2021-10-26 12:52:25 +0200

    Fix app

--- src/App.js
+++ src/App.js
@@ -36,12 +36,15 @@ const App = ()=> {
            <Grow in>
                <Container>
                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+
+                   <Grid item xs={12} sm={4}>
+                            <Form newPosts={newPosts}/>
+                        </Grid>
+                        
                         <Grid item xs={12} sm={7}>
                             <Posts posts={posts}/>
                         </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form newPosts={newPosts}/>
-                        </Grid>
+                        
                    </Grid>
                </Container>
            </Grow>
